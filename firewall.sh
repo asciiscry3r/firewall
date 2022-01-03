934b626bc9ce7eebc66ad5ae6f4af8d8de0c6e30
Maxim Klimenko Sergievich <81045684+asciiscry3r@users.noreply.github.com>
2022-01-03 04:17:44 +0200

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -140,36 +140,6 @@ ip6tables -A LOG_AND_DROP -j DROP
 ip6tables -A LOG_AND_REJECT -j LOG --log-prefix "ip6tables reject: " --log-level 7
 ip6tables -A LOG_AND_REJECT -j REJECT --reject-with icmp6-adm-prohibited
 
-# From rc.DMZ.firewall - DMZ IP Firewall script for Linux 2.4.x and iptables
-# Copyright (C) 2001  Oskar Andreasson <bluefluxATkoffeinDOTnet>
-# bad_tcp_packets chain
-ip6tables -A bad_tcp_packets -p tcp --tcp-flags SYN,ACK SYN,ACK \
--m state --state NEW -j REJECT --reject-with tcp-reset
-ip6tables -A bad_tcp_packets -p tcp ! --syn -m state --state NEW -j LOG \
---log-prefix "New not syn: "
-ip6tables -A bad_tcp_packets -p tcp ! --syn -m state --state NEW -j DROP
-
-ip6tables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-ip6tables -A INPUT -i lo -j bad_tcp_packets
-ip6tables -A OUTPUT -i lo -j bad_tcp_packets
-ip6tables -A INPUT -p ipv6-icmp --icmpv6-type 128 -m conntrack --ctstate NEW -j ACCEPT
-# ip6tables -A INPUT -s fe80::/10 -p ipv6-icmp -j ACCEPT
-# ip6tables -A INPUT -p udp --sport 547 --dport 546 -j ACCEPT
-ip6tables -A INPUT -m conntrack --ctstate INVALID -j LOG_AND_DROP
-ip6tables -A INPUT -p udp -m conntrack --ctstate NEW -j UDP
-ip6tables -A INPUT -p tcp --syn -m conntrack --ctstate NEW -j TCP
-ip6tables -A INPUT -p tcp -j bad_tcp_packets
-ip6tables -A INPUT -p tcp -m recent --set --rsource --name TCP-PORTSCAN -j REJECT --reject-with tcp-reset
-ip6tables -A INPUT -p udp -m recent --set --rsource --name UDP-PORTSCAN -j REJECT --reject-with icmp6-adm-prohibited
-ip6tables -A INPUT -p dccp -j LOG_AND_DROP
-ip6tables -A INPUT -p sctp -j LOG_AND_DROP
-ip6tables -A OUTPUT -p dccp -j LOG_AND_DROP
-ip6tables -A OUTPUT -p sctp -j LOG_AND_DROP
-ip6tables -A OUTPUT -p tcp -j bad_tcp_packets
-ip6tables -A OUTPUT -p udp --dport 547 -j LOG_AND_DROP
-# ip6tables -A OUTPUT -p tcp --dport 80 -j LOG_AND_DROP
-ip6tables -A OUTPUT -m limit --limit 3/minute --limit-burst 3 -j LOG --log-level DEBUG --log-prefix "IPT OUTPUT packet died: "
-
 # Ip lists
 ip6tables -A INPUT -s 2002::/24 -j LOG_AND_DROP
 ip6tables -A OUTPUT -s 2002::/24 -j LOG_AND_DROP
@@ -228,6 +198,36 @@ ip6tables -A INPUT -s 2001:0:f000::/36 -j LOG_AND_DROP
 ip6tables -A OUTPUT -s 2001:0:f000::/36 -j LOG_AND_DROP
 ip6tables -A INPUT -s 2001:0:ffff:ffff::/64 -j LOG_AND_DROP
 ip6tables -A OUTPUT -s 2001:0:ffff:ffff::/64 -j LOG_AND_DROP
+
+# From rc.DMZ.firewall - DMZ IP Firewall script for Linux 2.4.x and iptables
+# Copyright (C) 2001  Oskar Andreasson <bluefluxATkoffeinDOTnet>
+# bad_tcp_packets chain
+ip6tables -A bad_tcp_packets -p tcp --tcp-flags SYN,ACK SYN,ACK \
+-m state --state NEW -j REJECT --reject-with tcp-reset
+ip6tables -A bad_tcp_packets -p tcp ! --syn -m state --state NEW -j LOG \
+--log-prefix "New not syn: "
+ip6tables -A bad_tcp_packets -p tcp ! --syn -m state --state NEW -j DROP
+
+ip6tables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+ip6tables -A INPUT -i lo -j bad_tcp_packets
+ip6tables -A OUTPUT -i lo -j bad_tcp_packets
+ip6tables -A INPUT -p ipv6-icmp --icmpv6-type 128 -m conntrack --ctstate NEW -j ACCEPT
+# ip6tables -A INPUT -s fe80::/10 -p ipv6-icmp -j ACCEPT
+# ip6tables -A INPUT -p udp --sport 547 --dport 546 -j ACCEPT
+ip6tables -A INPUT -m conntrack --ctstate INVALID -j LOG_AND_DROP
+ip6tables -A INPUT -p udp -m conntrack --ctstate NEW -j UDP
+ip6tables -A INPUT -p tcp --syn -m conntrack --ctstate NEW -j TCP
+ip6tables -A INPUT -p tcp -j bad_tcp_packets
+ip6tables -A INPUT -p tcp -m recent --set --rsource --name TCP-PORTSCAN -j REJECT --reject-with tcp-reset
+ip6tables -A INPUT -p udp -m recent --set --rsource --name UDP-PORTSCAN -j REJECT --reject-with icmp6-adm-prohibited
+ip6tables -A INPUT -p dccp -j LOG_AND_DROP
+ip6tables -A INPUT -p sctp -j LOG_AND_DROP
+ip6tables -A OUTPUT -p dccp -j LOG_AND_DROP
+ip6tables -A OUTPUT -p sctp -j LOG_AND_DROP
+ip6tables -A OUTPUT -p tcp -j bad_tcp_packets
+ip6tables -A OUTPUT -p udp --dport 547 -j LOG_AND_DROP
+# ip6tables -A OUTPUT -p tcp --dport 80 -j LOG_AND_DROP
+ip6tables -A OUTPUT -m limit --limit 3/minute --limit-burst 3 -j LOG --log-level DEBUG --log-prefix "IPT OUTPUT packet died: "
 
 # ip6tables -A INPUT -m set --match-set bogonsv6 src -j LOG_AND_DROP
 # ip6tables -A OUTPUT -m set --match-set bogonsv6 src -j LOG_AND_DROP
