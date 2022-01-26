02c0b3f0a32abea3ec262e5ef9019c9073a6c93f
Maxim Klimenko Sergievich <81045684+asciiscry3r@users.noreply.github.com>
2022-01-26 22:22:21 +0200

    Added new link and rules

--- firewall.sh
+++ firewall.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # Klimenko Maxim Sergievich 2022
 # https://www.frozentux.net/iptables-tutorial/iptables-tutorial.html#PROTOCOLSTXT
+# https://github.com/ukanth/afwall/wiki/CustomScripts#droidwall-only-examples
 # https://wiki.archlinux.org/title/simple_stateful_firewall
 # https://ipgeolocation.io/resources/bogon.html
 
@@ -102,7 +103,9 @@ iptables -A INPUT -p icmp --icmp-type 8 -m conntrack --ctstate NEW -j ACCEPT
 # iptables -A IN_SSH -m recent --name sshbf --rttl --rcheck --hitcount 3 --seconds 10 -j LOG_AND_DROP
 # iptables -A IN_SSH -m recent --name sshbf --rttl --rcheck --hitcount 4 --seconds 1800 -j LOG_AND_DROP 
 # iptables -A IN_SSH -m recent --name sshbf --set -j ACCEPT
-iptables -A INPUT -m string --algo bm --hex-string '|28 29 20 7B|' -j DROP
+iptables -A INPUT -m string --algo bm --hex-string '|28 29 20 7B|' -j LOG_AND_DROP
+iptables -A INPUT -p icmp -m icmp --icmp-type address-mask-request -j LOG_AND_DROP
+iptables -A INPUT -p icmp -m icmp --icmp-type timestamp-request -j LOG_AND_DROP
 iptables -A INPUT -m conntrack --ctstate INVALID -j LOG_AND_DROP
 iptables -A INPUT -p udp -m conntrack --ctstate NEW -j UDP
 iptables -A INPUT -p tcp --syn -m conntrack --ctstate NEW -j TCP
@@ -244,7 +247,9 @@ ip6tables -A INPUT -i lo -j ACCEPT
 ip6tables -A INPUT -p ipv6-icmp --icmpv6-type 128 -m conntrack --ctstate NEW -j ACCEPT
 # ip6tables -A INPUT -s fe80::/10 -p ipv6-icmp -j ACCEPT
 # ip6tables -A INPUT -p udp --sport 547 --dport 546 -j ACCEPT
-ip6tables -A INPUT -m string --algo bm --hex-string '|28 29 20 7B|' -j DROP
+ip6tables -A INPUT -m string --algo bm --hex-string '|28 29 20 7B|' -j LOG_AND_DROP
+ip6tables -A INPUT -p icmp -m icmp --icmp-type address-mask-request -j LOG_AND_DROP
+ip6tables -A INPUT -p icmp -m icmp --icmp-type timestamp-request -j LOG_AND_DROP
 ip6tables -A INPUT -m conntrack --ctstate INVALID -j LOG_AND_DROP
 ip6tables -A INPUT -p udp -m conntrack --ctstate NEW -j UDP
 ip6tables -A INPUT -p tcp --syn -m conntrack --ctstate NEW -j TCP
