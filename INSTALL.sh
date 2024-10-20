0e311c530f8ea569547275018bf6a0f23a372118
Klimenko Maxim Sergievich <klimenkomaximsergievich@gmail.com>
2024-10-20 20:25:54 +0300

    Added sysctl settings

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,28 +1,43 @@
 #!/usr/bin/env bash
 
 function install_simplestatefulfirewall {
-  yes | sudo cp -rf simplestatefulfirewall.service /usr/lib/systemd/system/simplestatefulfirewall.service
-  yes | sudo cp -rf simplestatefulfirewall.timer /usr/lib/systemd/system/simplestatefulfirewall.timer
-  yes | sudo cp -rf simplestatefulfirewall.sh /usr/bin/simplestatefulfirewall.sh
-  chmod 0640 /usr/lib/systemd/system/simplestatefulfirewall.service
-  chmod 0640 /usr/lib/systemd/system/simplestatefulfirewall.timer
-  chmod u=rwx,g=rx /usr/bin/simplestatefulfirewall.sh
-  systemctl daemon-reload
+    yes | sudo cp -rf simplestatefulfirewall.service /usr/lib/systemd/system/simplestatefulfirewall.service
+    yes | sudo cp -rf simplestatefulfirewall.timer /usr/lib/systemd/system/simplestatefulfirewall.timer
+    yes | sudo cp -rf simplestatefulfirewall.sh /usr/bin/simplestatefulfirewall.sh
+    chmod 0640 /usr/lib/systemd/system/simplestatefulfirewall.service
+    chmod 0640 /usr/lib/systemd/system/simplestatefulfirewall.timer
+    chmod u=rwx,g=rx /usr/bin/simplestatefulfirewall.sh
+    systemctl daemon-reload
 }
 
-if [ ! -f /usr/lib/systemd/system/simplestatefulfirewall.service ]; then
-    install_simplestatefulfirewall
-fi
+function install_settingstosysctl {
+    yes | sudo cp -rf sysctl.conf /etc/sysctl.conf
+    sysctl -p /etc/sysctl.conf
+    interfaces=(`sudo  sysctl -a | grep accept_redirects | awk 'BEGIN{FS="."} {print $4}'`)
 
-if [ ! -f /usr/lib/systemd/system/simplestatefulfirewall.timer ]; then
-    install_simplestatefulfirewall
-    systemctl start simplestatefulfirewall.timer
-    systemctl enable simplestatefulfirewall.timer
-else
-    systemctl start simplestatefulfirewall.timer
-    systemctl enable simplestatefulfirewall.timer
-fi
+    for i in "${interfaces[@]}"; do
+	sudo sysctl -w net.ipv4.conf."${i}".forwarding=0
+	sudo sysctl -w net.ipv6.conf."${i}".forwarding=0
+	sudo sysctl -w net.ipv4.conf."${i}".rp_filter=1
+	sudo sysctl -w net.ipv4.conf."${i}".accept_redirects=0
+	sudo sysctl -w net.ipv4.conf."${i}".secure_redirects=0
+	sudo sysctl -w net.ipv4.conf."${i}".send_redirects=0
+	sudo sysctl -w net.ipv4.conf."${i}".accept_source_route=0
+	sudo sysctl -w net.ipv6.conf."${i}".accept_redirects=0
+	sudo sysctl -w net.ipv6.conf."${i}".accept_source_route=0
+	sudo sysctl -w net.ipv4.conf."${i}".bootp_relay=0
+	sudo sysctl -w net.ipv4.conf."${i}".proxy_arp=0
+	sudo sysctl -w net.ipv4.conf."${i}".arp_ignore=1
+        sudo sysctl -w net.ipv4.conf."${i}".arp_announce=2
+	sudo sysctl -w net.ipv4.conf."${i}".log_martians=1
+	sudo sysctl -w net.ipv6.conf."${i}".autoconf=0
+        sudo sysctl -w net.ipv6.conf."${i}".accept_ra=0
+	sudo sysctl -w net.ipv6.conf."${i}".use_tempaddr=2
+	sudo sysctl -w net.ipv6.conf."${i}".rpl_seg_enabled=0
+	sudo sysctl -w net.ipv6.conf."${i}".disable_ipv6=1
+    done
+}
 
-if [ ! -f /usr/bin/simplestatefulfirewall.sh ]; then
-    install_simplestatefulfirewall
-fi
+install_settingstosysctl
+
+install_simplestatefulfirewall
